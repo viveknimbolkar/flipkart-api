7911b5d25075914a81044267e0358bba16dd78c8
viveknimbolkar <viveknimbolkar50@gmail.com>
2023-01-23 21:22:47 +0530

    initiate the connection with the database

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
 const userRoute = require("./src/routes/user");
+const db = require("./src/database/database");
 
-
+// Middlewares
 app.use(cors());
 app.use(cookieParser());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 app.use(userRoute);
-
 
 app.listen(process.env.PORT, () => {
   console.log("listening on port: ", process.env.PORT);
