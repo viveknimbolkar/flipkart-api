678e9960c39fffedbac8e5510206810c414b392c
viveknimbolkar <viveknimbolkar50@gmail.com>
2023-02-10 10:36:30 +0530

    add route to remove cart item

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -75,6 +75,45 @@ router.post("/add_address", verifyUser, async (req, res) => {
 
 router.post("/get_cart_items", verifyUser, async (req, res) => {});
 
+router.post("/remove_cart_item", verifyUser, async (req, res) => {
+  const token = jwt.decode(req.headers.authorization, process.env.JWT_SECRET);
+  const {
+    id, // product id
+    name, // product name
+  } = req.body;
+
+  if (!name || !id) {
+    res.status(400).json({ message: "empty fields are not allowed" });
+  }
+
+  Cart.findByIdAndDelete(id, async (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("deleted from cart collection");
+      // remove from user collection as well
+      const filter = { email: token.email };
+      const updateCart = {
+        $pull: {
+          cart: {
+            $in: [id],
+          },
+        },
+      };
+      const removeCartID = await Customer.updateOne(filter, updateCart);
+      if (removeCartID) {
+        res.status(200).json({
+          message: "cart item removed successfuly",
+        });
+      } else {
+        res
+          .status(200)
+          .json({ message: "something went wrong! try again later" });
+      }
+    }
+  });
+});
+
 router.post("/add_to_cart", verifyUser, async (req, res) => {
   const token = jwt.decode(req.headers.authorization, process.env.JWT_SECRET);
   const {
