cd99d60a2168b5d0617a208b11cf0b2c0d9e8532
viveknimbolkar <viveknimbolkar50@gmail.com>
2023-02-22 14:52:58 +0530

    add auth for seller

---
+++ src/model/seller.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+
+// main user schema
+const seller = new Schema({
+  name: {
+    require: true,
+    type: String,
+  },
+  email: {
+    require: true,
+    type: String,
+  },
+  password: {
+    require: true,
+    type: String,
+  },
+});
+
+const Seller = mongoose.model("Seller", seller);
+
+module.exports = { Seller };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,6 +5,7 @@ const jwt = require("jsonwebtoken");
 const db = require("../database/database");
 const { Customer } = require("../model/user");
 const nodemailer = require("nodemailer");
+const { Seller } = require("../model/seller");
 
 /**
  * @swagger
@@ -21,12 +22,17 @@ router.get("/", async (req, res) => {
 
 /**
  * @swagger
- * /login:
+ * /customer/login:
  *   post:
  *     description: login the user
  *     parameters:
- *       - in: formData
- *         email: email
+ *       - in: query
+ *         name: email
+ *         schema:
+ *           type: string
+ *         required: true
+ *       - in: query
+ *         name: password
  *         schema:
  *           type: string
  *         required: true
@@ -36,7 +42,7 @@ router.get("/", async (req, res) => {
  *       500:
  *         description: Error
  */
-router.post("/login", async (req, res) => {
+router.post("/customer/login", async (req, res) => {
   Customer.find({ email: req.body.email })
     .exec()
     .then((customer) => {
@@ -92,7 +98,84 @@ router.post("/login", async (req, res) => {
 
 /**
  * @swagger
- * /register:
+ * /seller/login:
+ *   post:
+ *     description: login the seller
+ *     parameters:
+ *       - in: query
+ *         name: email
+ *         schema:
+ *           type: string
+ *         required: true
+ *       - in: query
+ *         name: password
+ *         schema:
+ *           type: string
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Error
+ */
+router.post("/seller/login", async (req, res) => {
+  Seller.find({ email: req.body.email })
+    .exec()
+    .then((seller) => {
+      if (seller.length < 1) {
+        return res.status(401).json({
+          message: "Invalid email or password!",
+        });
+      }
+
+      // compare normal string
+      if (req.body.password === seller[0].password) {
+        const payload = {
+          email: seller[0].email,
+          name: seller[0].name,
+        };
+        const token = jwt.sign(payload, process.env.JWT_SECRET);
+
+        return res.status(200).json({
+          message: "login successful",
+          token: token,
+        });
+      }
+      // store passwords with encryption
+      // bcrypt.compare(req.body.password, customer[0].password, (err, result) => {
+      //   console.log(result);
+      //   if (!result) {
+      //     res.status(401).json({
+      //       message: "Incorrect email or password",
+      //     });
+      //   }
+      //   if (result) {
+      //     const payload = {
+      //       email: customer[0].email,
+      //       name: customer[0].name,
+      //     };
+      //     const token = jwt.sign(payload, process.env.JWT_SECRET);
+
+      //     res.status(200).json({
+      //       message: "login successful",
+      //       token: token,
+      //     });
+      //   }
+      // });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "server unreachable",
+      });
+      res.end();
+    });
+});
+
+
+/**
+ * @swagger
+ * /seller/register:
  *   post:
  *     description: register a new user
  *     parameters:
@@ -117,7 +200,60 @@ router.post("/login", async (req, res) => {
  *       500:
  *         description: Error
  */
-router.post("/register", async (req, res) => {
+router.post("/seller/register", async (req, res) => {
+  var { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.json({
+      message: "Empty fields are not allowed",
+    });
+    res.end();
+  }
+
+  const customerData = {
+    name: name,
+    email: email,
+    password: password,
+  };
+
+  const seller = new Seller(customerData);
+  await seller.save();
+
+  res.status(200).json({
+    message: "seller registered successfully",
+  });
+  res.end();
+});
+
+
+/**
+ * @swagger
+ * /customer/register:
+ *   post:
+ *     description: register a new user
+ *     parameters:
+ *       - in: query
+ *         name: name
+ *         schema:
+ *           type: string
+ *         required: true
+ *       - in: query
+ *         name: email
+ *         schema:
+ *           type: string
+ *         required: true
+ *       - in: query
+ *         name: password
+ *         schema:
+ *           type: string
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Error
+ */
+router.post("/customer/register", async (req, res) => {
   var { name, email, password } = req.body;
 
   if (!name || !email || !password) {
